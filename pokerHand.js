86fe45c14dcd82177339fa235be5cf40cfa5a52d
Abdulrahman Moussa <abdimoussa98@gmail.com>
2023-01-10 15:54:46 -0600

    mob - end of first  mob session

--- pokerHand.js
+++ pokerHand.js
@@ -1,7 +1,43 @@
 'use strict';
 
-function answer() {
-  return 6 * 9;
+let sortCards = (a, b) => {
+  let cardValueA = getCardValue(a)
+  let cardValueB = getCardValue(b);
+
+  if (cardValueA === cardValueB) {
+    return 0
+  }
+
+  if (cardValueA > cardValueB) {
+    return 1
+  }
+
+  return -1
+};
+
+function getCardValue(card){
+  let cardValue = card.slice(0, card.length - 1)
+  if(cardValue == 'K'){
+    return 13
+  } else {
+    return parseInt(cardValue)
+  }
+
+}
+
+function answer(blackHand, whiteHand) {
+  let blackCards = blackHand.split(' ');
+  let whiteCards = whiteHand.split(' ');
+
+  const sortedBlackCards = blackCards.sort(sortCards)
+  const sortedWhiteCards = whiteCards.sort(sortCards)
+  console.log(sortedBlackCards)
+
+  if (getCardValue(sortedBlackCards[4]) > getCardValue(sortedWhiteCards[4])) {
+    return  `Black wins - high card: ${sortedBlackCards[4].slice(0, sortedBlackCards[4].length - 1)}`;
+  } else {
+    return  `White wins - high card: ${sortedWhiteCards[4].slice(0, sortedWhiteCards[4].length - 1)}`;
+  }
 }
 
 module.exports = answer;
--- pokerHand.test.js
+++ pokerHand.test.js
@@ -2,8 +2,17 @@
 
 const answer = require('./pokerHand');
 
-describe('answer', () => {
-  it('to life the universe and everything', () => {
-    expect(answer()).toEqual(42);
+describe('poker hands', () => {
+  it('high card should win', () => {
+    expect(answer('2H 3D 5S 9C 9D', '2C 3H 4S 8C 7H')).toEqual('Black wins - high card: 9');
   });
+
+  it('face card beats number', () => {
+    expect(answer('2H 3D 5S 9C 10D', '2C 3H KS 8C 9H')).toEqual('White wins - high card: K');
+  })
+  it('handle high card of 10', () => {
+    expect(answer('2H 3D 5S 9C 10D', '2C 3H 5S 8C 9H')).toEqual('Black wins - high card: 10');
+  })
+
+  // todo: handle 10, J, Q, K, A
 });
