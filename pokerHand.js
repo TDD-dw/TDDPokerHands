5ab5256c7390128a53c714035ac59764a0136805
Nina Brissey <nina-brissey@Ninas-MBP.attlocal.net>
2023-01-10 16:03:28 -0600

    Write test for compare hands and start implimentatio

--- pokerHand.js
+++ pokerHand.js
@@ -1,7 +1,11 @@
 'use strict';
 
-function answer() {
-  return 6 * 9;
+function compareHands(black, white) {
+  // '2H 3D 5S 9C KD', 2:H, 3:D,
+  // '2C 3H 4S 8C AH'
+  let blackCards = black.split(' ').map((card) => {});
+  let whiteCards = white.split(' ').map();
+  return 'White wins - high card: Ace';
 }
 
-module.exports = answer;
+module.exports = { compareHands };
--- pokerHand.test.js
+++ pokerHand.test.js
@@ -1,9 +1,13 @@
 'use strict';
 
-const answer = require('./pokerHand');
+const{compareHands} = require('./pokerHand.js');
 
 describe('answer', () => {
-  it('to life the universe and everything', () => {
-    expect(answer()).toEqual(42);
+  it('High Card; White Wins', () => {
+    expect(compareHands('2H 3D 5S 9C KD', '2C 3H 4S 8C AH')).toMatch('White wins - high card: Ace');
   });
+
+
+  //   Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH
+  //   White wins - high card: Ace
 });
