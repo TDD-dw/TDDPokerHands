859e6feb92f708a6b1e52d3a5664876bcb88a6c3
Jonah Price <jyoonah@gmail.com>
2023-01-12 12:41:47 -0800

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:pokerHand.test.js

--- pokerHand.js
+++ pokerHand.js
@@ -3,8 +3,8 @@
 function compareHands(black, white) {
   // '2H 3D 5S 9C KD', 2:H, 3:D,
   // '2C 3H 4S 8C AH'
-  let blackCards = black.split(' ').map((card) => {});
-  let whiteCards = white.split(' ').map();
+  // let blackCards = black.split(' ').map((card) => {});
+  // let whiteCards = white.split(' ').map();
   return 'White wins - high card: Ace';
 }
 
--- pokerHand.test.js
+++ pokerHand.test.js
@@ -4,8 +4,28 @@ const{compareHands} = require('./pokerHand.js');
 
 describe('answer', () => {
   it('High Card; White Wins', () => {
-    expect(compareHands('2H 3D 5S 9C KD', '2C 3H 4S 8C AH')).toMatch('White wins - high card: Ace');
+    const datasetOne = {
+      inputData: [
+        '2H 3D 5S 9C KD', 
+        '2C 3H 4S 8C AH'
+      ],
+      expectedOutput: 'White wins - high card: Ace'
+    }
+    expect(compareHands(datasetOne.inputData))
+    .toMatch(datasetOne.expectedOutput);
   });
+
+  it('High Card; Black Wins', () => {
+    const datasetOne = {
+      inputData: [
+        '2C 3H 4S 8C AH',
+        '2H 3D 5S 9C KD'
+      ],
+      expectedOutput: 'Black wins - high card: Ace'
+    }
+    expect(compareHands(datasetOne.inputData))
+    .toMatch(datasetOne.expectedOutput)
+  })
 
 
   //   Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH
