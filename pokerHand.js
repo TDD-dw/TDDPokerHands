ece31d5cce7162fe23079770c917082ab62af010
Nina Brissey <nina-brissey@Ninas-MBP.attlocal.net>
2023-01-12 15:04:45 -0600

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:pokerHand.js

--- pokerHand.js
+++ pokerHand.js
@@ -1,11 +1,24 @@
 'use strict';
 
 function compareHands(black, white) {
-  // '2H 3D 5S 9C KD', 2:H, 3:D,
+  // '2H 3D 5S 9C KD',
   // '2C 3H 4S 8C AH'
-  // let blackCards = black.split(' ').map((card) => {});
-  // let whiteCards = white.split(' ').map();
+
+  // read the hands
+  // sort hand
+  // getting highest value for each hand
+  // comparing the values
+  // output winner
+
   return 'White wins - high card: Ace';
+}
+
+function readHand(hand) {
+  let readHand = hand.split(' ').map((newCard) => {
+    {
+      newCard[0] = newCard[1];
+    }
+  });
 }
 
 module.exports = { compareHands };
