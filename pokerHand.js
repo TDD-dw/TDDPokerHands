64b371da7d3d67a11155fa917f11241195d576d9
danielcohen <decohen2@gmail.com>
2023-01-19 11:45:25 -0800

    mob session 1/19/22

--- pokerHand.js
+++ pokerHand.js
@@ -1,86 +1,76 @@
-'use strict';
+"use strict";
 
 let faceCardValues = {
   J: 11,
   Q: 12,
   K: 13,
   A: 14,
-}
+};
 
+// LOOK INTO SORTING REVERSE???
 let sortCards = (a, b) => {
-  let cardValueA = getCardValue(a)
+  let cardValueA = getCardValue(a);
   let cardValueB = getCardValue(b);
 
   if (cardValueA === cardValueB) {
-    return 0
+    return 0;
   }
 
   if (cardValueA > cardValueB) {
-    return 1
+    return 1;
   }
 
-  return -1
+  return -1;
 };
 
 function getCardValue(card) {
-  let cardValue = card.slice(0, card.length - 1)
-  return faceCardValues[cardValue] ?? parseInt(cardValue)
+  let cardValue = card.slice(0, card.length - 1);
+  return faceCardValues[cardValue] ?? parseInt(cardValue);
 }
 
+function evaluateHand(hand) {
+  const sortedCards = hand.sort(sortCards);
 
-
-function evaluateHand(hand){
-const sortedCards = hand.sort(sortCards)
-
-sortedCards.slice(0, 4).map((card, index) => {
-  if (getCardValue(card) === getCardValue(sortedCards[index + 1])) {
-    return {
-      handType: "pair",
-      handValue: 1000 + getCardValue(card),
-      highCards: []
+  for (let i = 0; i < sortedCards.length - 1; i++) {
+    if (getCardValue(sortedCards[i]) === getCardValue(sortedCards[i + 1])) {
+      return {
+        handType: "pair",
+        handValue: 1000 + getCardValue(sortedCards[i]),
+        highCards: [sortedCards[i + 2]],
+      };
     }
   }
-})
+
   return {
     handType: "high card",
     handValue: 0,
-    highCards: sortedCards
-  }
+    highCards: sortedCards,
+  };
+}
+
+function getCardValueString(card) {
+  return card.slice(0, card.length - 1);
 }
 
 function answer(player1Hand, player2Hand) {
-  let player1Cards = player1Hand.split(' ');
-  let player1Evaluated = evaluateHand(player1Cards)
-  let player2Cards = player2Hand.split(' ');
-  let player2Evaluated = evaluateHand(player2Cards)
-  let hasPair = false;
-  let player2HasPair = false;
-  
-  
-  const sortedPlayer2Cards = player2Cards.sort(sortCards)
+  let player1Cards = player1Hand.split(" ");
+  let player1Evaluated = evaluateHand(player1Cards);
+  let player2Cards = player2Hand.split(" ");
+  let player2Evaluated = evaluateHand(player2Cards);
 
-  
-
-  sortedPlayer2Cards.slice(0, 4).map((card, index) => {
-    if (getCardValue(card) === getCardValue(sortedPlayer2Cards[index + 1])) {
-      player2HasPair = true;
-    }
-  })
-
-     
-  if (hasPair) {
-    return 'Player 1 wins - pair';
-  }
-  if (player2HasPair) {
-    return 'Player 2 wins - pair';
-  }
-
-  if (getCardValue(sortedPlayer1Cards[4]) > getCardValue(sortedPlayer2Cards[4])) {
-    return `Player 1 wins - high card: ${sortedPlayer1Cards[4].slice(0, sortedPlayer1Cards[4].length - 1)}`;
+  if (player1Evaluated.handValue > player2Evaluated.handValue) {
+    return "Player 1 wins - " + player1Evaluated.handType;
+  } else if (player2Evaluated.handValue > player1Evaluated.handValue) {
+    return "Player 2 wins - " + player2Evaluated.handType;
   } else {
-    return `Player 2 wins - high card: ${sortedPlayer2Cards[4].slice(0, sortedPlayer2Cards[4].length - 1)}`;
+    let p1HighCard = player1Evaluated.highCards[0];
+    let p2HighCard = player2Evaluated.highCards[0];
+    if (getCardValue(p1HighCard) > getCardValue(p2HighCard)) {
+      return `Player 1 wins - high card: ${getCardValueString(p1HighCard)}`;
+    } else {
+      return `Player 2 wins - high card: ${getCardValueString(p2HighCard)}`;
+    }
   }
 }
-
 
 module.exports = answer;
--- pokerHand.test.js
+++ pokerHand.test.js
@@ -1,49 +1,78 @@
-'use strict';
+"use strict";
 
-const answer = require('./pokerHand');
+const answer = require("./pokerHand");
 
-describe('poker hands', () => {
-  describe('high cards', () => {
-    it('high card should win', () => {
-      expect(answer('2H 3D 5S 9C 7D', '2C 3H 4S 8C 7H')).toEqual('Player 1 wins - high card: 9');
+describe("poker hands", () => {
+  describe("high cards", () => {
+    it("high card should win", () => {
+      expect(answer("2H 3D 5S 9C 7D", "2C 3H 4S 8C 7H")).toEqual(
+        "Player 1 wins - high card: 9"
+      );
     });
 
-    it('face card beats number', () => {
-      expect(answer('2H 3D 5S 9C 10D', '2C 3H KS 8C 9H')).toEqual('Player 2 wins - high card: K');
-      expect(answer('2H 3D 5S 9C 10D', '2C 3H QS 8C 9H')).toEqual('Player 2 wins - high card: Q');
-      expect(answer('2H 3D 5S 9C 10D', '2C 3H JS 8C 9H')).toEqual('Player 2 wins - high card: J');
-      expect(answer('2H 3D 5S 9C 10D', '2C 3H AS 8C 9H')).toEqual('Player 2 wins - high card: A');
-    })
+    it("face card beats number", () => {
+      expect(answer("2H 3D 5S 9C 10D", "2C 3H KS 8C 9H")).toEqual(
+        "Player 2 wins - high card: K"
+      );
+      expect(answer("2H 3D 5S 9C 10D", "2C 3H QS 8C 9H")).toEqual(
+        "Player 2 wins - high card: Q"
+      );
+      expect(answer("2H 3D 5S 9C 10D", "2C 3H JS 8C 9H")).toEqual(
+        "Player 2 wins - high card: J"
+      );
+      expect(answer("2H 3D 5S 9C 10D", "2C 3H AS 8C 9H")).toEqual(
+        "Player 2 wins - high card: A"
+      );
+    });
 
-    it('handle high card of 10', () => {
-      expect(answer('2H 3D 5S 9C 10D', '2C 3H 5S 8C 9H')).toEqual('Player 1 wins - high card: 10');
-    })
+    it("handle high card of 10", () => {
+      expect(answer("2H 3D 5S 9C 10D", "2C 3H 5S 8C 9H")).toEqual(
+        "Player 1 wins - high card: 10"
+      );
+    });
 
-    it('king beats queen', () => {
-      expect(answer('2H 3D KS 9C 10D', '2C 3H QS 8C 9H')).toEqual('Player 1 wins - high card: K');
-    })
+    it("king beats queen", () => {
+      expect(answer("2H 3D KS 9C 10D", "2C 3H QS 8C 9H")).toEqual(
+        "Player 1 wins - high card: K"
+      );
+    });
 
-    it('queen beats jack', () => {
-      expect(answer('2H 3D JS 9C 10D', '2C 3H QS 8C 9H')).toEqual('Player 2 wins - high card: Q');
-    })
+    it("queen beats jack", () => {
+      expect(answer("2H 3D JS 9C 10D", "2C 3H QS 8C 9H")).toEqual(
+        "Player 2 wins - high card: Q"
+      );
+    });
 
-    it('ace beats king', () => {
-      expect(answer('2H 3D AS 9C 10D', '2C 3H KS 8C 9H')).toEqual('Player 1 wins - high card: A');
-    })
-  })
+    it("ace beats king", () => {
+      expect(answer("2H 3D AS 9C 10D", "2C 3H KS 8C 9H")).toEqual(
+        "Player 1 wins - high card: A"
+      );
+    });
+  });
 
-  describe('pairs', () => {
-    it('any pair beats any high card', () => {
-      expect(answer('2H 2D AS 9C 10D', '2C 3H KS 8C 9H')).toEqual('Player 1 wins - pair');
-      expect(answer('2C 3H KS 8C 9H', '2H 2D AS 9C 10D')).toEqual('Player 2 wins - pair');
-    })
+  describe("pairs", () => {
+    it("any pair beats any high card", () => {
+      expect(answer("2H 2D AS 9C 10D", "2C 3H KS 8C 9H")).toEqual(
+        "Player 1 wins - pair"
+      );
+      expect(answer("2C 3H KS 8C 9H", "2H 2D AS 9C 10D")).toEqual(
+        "Player 2 wins - pair"
+      );
+    });
 
-    it.skip('High pair beats low pair', () => {
-      expect(answer('8H 8D AS 9C 10D', '2C 2H KS 8C 9H')).toEqual('Player 1 wins - high pair');
-    })
-  })
+    it("High pair beats low pair", () => {
+      expect(answer("8H 8D AS 9C 10D", "2C 2H KS 8C 9H")).toEqual(
+        "Player 1 wins - pair"
+      );
+    });
+    it("When pairs are tied then high card wins", () => {
+      debugger;
+      expect(answer("8H 8D AS 9C 10D", "8C 8H KS 3C 9H")).toEqual(
+        "Player 1 wins - pair with high card: A"
+      );
+    });
+  });
 
-  it.todo('error cases')
-  it.todo('tie cases')
-
+  it.todo("error cases");
+  it.todo("tie cases");
 });
