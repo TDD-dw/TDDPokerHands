df5dca9bcfa92c95242452f4c3d42b968ed30976
Devon Wigle <devon.wigle@thoughtworks.com>
2023-01-20 10:43:19 -0600

    Corrected merge conflicts

--- pokerHand.js
+++ pokerHand.js
@@ -1,7 +1,6 @@
 'use strict';
 
 function compareHands(black, white) {
-
   // '2H 3D 5S 9C KD',
   // '2C 3H 4S 8C AH'
 
@@ -10,26 +9,45 @@ function compareHands(black, white) {
   // getting highest value for each hand
   // comparing the values
   // output winner
+  let readBlack = sortHand(convertFaceCards(readHand(black)))
+  let readWhite = readHand(white)
 
   return 'White wins - high card: Ace';
 }
 
 function readHand(hand) {
 
-   let handValues = hand.split(' ')
-   let handValueObjects = handValues.map( newCard => {
-     const cardObject = {
-       [newCard[0]] : newCard[1]
-     }
-     return cardObject
-   })
+  let handValues = hand.split(' ')
+  let handValueObjects = handValues.map( newCard => {
+    const cardObject = {
+      num: newCard[0], suite: newCard[1]
+    }
+    return cardObject
+  })
   return handValueObjects
 
 }
-// TODO create method change A,J,Q,K to respective number value
+
+function convertFaceCards(hands){
+    const faceValues = {J : 11, Q : 12, K : 13, A : 14}
+    var convertedHand = []
+    let faceValuesKeys = Object.keys(faceValues)
+
+    hands.forEach(card => {
+      if (faceValuesKeys.includes(card.num)) {
+          convertedHand.push(card)
+          card.num = faceValues[card.num]
+      } else {
+        convertedHand.push(card)
+      }
+      })
+
+    return convertedHand
+}
+
 function sortHand(hands) {
     const sortedHandValues = hands.sort((a,b) => (a.num > b.num ? 1:-1))
     return sortedHandValues
 }
 
-module.exports = { compareHands, readHand, sortHand };
+module.exports = { compareHands, readHand, convertFaceCards, sortHand };
--- pokerHand.test.js
+++ pokerHand.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const{compareHands, readHand, sortHand} = require('./pokerHand.js');
+const{compareHands, readHand, convertFaceCards, sortHand} = require('./pokerHand.js');
 
 describe('answer', () => {
   it('High Card; White Wins', () => {
@@ -15,27 +15,30 @@ describe('answer', () => {
     .toMatch(datasetOne.expectedOutput);
   });
 
-//  it('High Card; Black Wins', () => {
-//    const datasetOne = {
-//      inputData: [
-//        '2C 3H 4S 8C AH',
-//        '2H 3D 5S 9C KD'
-//      ],
-//      expectedOutput: 'Black wins - high card: Ace'
-//    }
-//    expect(compareHands(datasetOne.inputData))
-//    .toMatch(datasetOne.expectedOutput)
-//  });
+  it('High Card; Black Wins', () => {
 
-   it('returns array of objects representing current hand', () => {
+      let black = '2C 3H 4S 8C AH'
+      let white = '2H 3D 5S 9C KD'
+      let expectedOutput = 'Black wins - high card: Ace'
+    expect(compareHands(black, white))
+    .toEqual(expectedOutput)
+  });
+
+  it('returns array of objects representing current hand', () => {
     const inputHand = '2C 3H 4S 8C AH'
-    const expectedOutput = [{2:'C'},{3:'H'},{4:'S'},{8:'C'},{A:'H'}]
+    const expectedOutput = [{ num: "2", suite: 'C' }, { num: "3", suite: 'H' }, { num: "4", suite: 'S' }, { num: "8", suite: 'C' }, { num: 'A', suite: 'H' }]
 
     expect(readHand(inputHand))
     .toEqual(expectedOutput)
 
   });
 
+  it('returns numerical value associated with face cards and aces', () =>{
+    const inputHandUnconverted = [{num:4, suite:'S'},{num:8, suite:'C'},{num:3, suite :'H'},{num:2, suite:'C'},{num : 'A', suite:'H'}]
+    const expectedHandConverted = [{num:4, suite:'S'},{num:8, suite:'C'},{num:3, suite :'H'},{num:2, suite:'C'},{num :14, suite:'H'}]
+
+    expect(convertFaceCards(inputHandUnconverted)).toEqual(expectedHandConverted)
+  });
 
     it('sorts array by value', () =>{
         const inputHandUnsorted = [{num:4, suite:'S'},{num:8, suite:'C'},{num:3, suite :'H'},{num:2, suite:'C'},{num :14, suite:'H'}]
@@ -44,7 +47,6 @@ describe('answer', () => {
         expect(sortHand(inputHandUnsorted))
         .toEqual(expectedHandSorted)
     })
-
 
   //   Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH
   //   White wins - high card: Ace
